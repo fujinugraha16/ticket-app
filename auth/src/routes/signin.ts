ca5c4d3cd5df7568e70330e71434fd9bdc0efa0e
fujinugraha16 <fujinugraha16@gmail.com>
2021-08-21 18:53:07 +0700

    common request validation middlewares

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,8 +1,11 @@
 import express, { Request, Response } from "express";
-import { body, validationResult } from "express-validator";
+import { body } from "express-validator";
 
 // erros
 import { RequestValidationError } from "../errors/request-validation-error";
+
+// middlewares
+import { validateRequest } from "../middlewares/validate-request";
 
 const router = express.Router();
 
@@ -15,12 +18,8 @@ router.post(
       .notEmpty()
       .withMessage("You must supply a password"),
   ],
-  async (req: Request, res: Response) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      throw new RequestValidationError(errors.array());
-    }
-  }
+  validateRequest,
+  async (req: Request, res: Response) => {}
 );
 
 export { router as signinUserRouter };
