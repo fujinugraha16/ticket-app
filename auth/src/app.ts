8eb3d1b99d278f6d065dc0c1b682383274dbb462
fujinugraha16 <fujinugraha16@gmail.com>
2021-08-21 22:25:03 +0700

    index to app refactor

---
+++ auth/src/app.ts
@@ -0,0 +1,40 @@
+import express from "express";
+import "express-async-errors";
+import { json } from "body-parser";
+import mongoose from "mongoose";
+import cookieSession from "cookie-session";
+
+// routers
+import { currentUserRouter } from "./routes/current-user";
+import { signinUserRouter } from "./routes/signin";
+import { signoutRouter } from "./routes/signout";
+import { signupRouter } from "./routes/signup";
+
+// middlewares
+import { errorHandler } from "./middlewares/error-handler";
+
+// errors
+import { NotFoundError } from "./errors/not-found-error";
+
+const app = express();
+app.set("trust proxy", true);
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true,
+  })
+);
+
+app.use(currentUserRouter);
+app.use(signinUserRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
+
+app.all("*", async () => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
