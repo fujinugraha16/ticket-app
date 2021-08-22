7e44635da9ba80a9d168eb0165b4cdd1f04d4f46
fujinugraha16 <fujinugraha16@gmail.com>
2021-08-22 13:42:14 +0700

    passing through cookies

--- client/pages/index.js
+++ client/pages/index.js
@@ -6,14 +6,15 @@ const LandingPage = ({ currentUser }) => {
   return <h1>Landing Page</h1>;
 };
 
-export const getServerSideProps = async () => {
+export const getServerSideProps = async ({ req }) => {
   const { data } = await axios.get(
     // http://SERVICE_NAME.NAMESPACE.svc.cluster.local/
     "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser",
     {
-      headers: {
-        Host: "ticketing.dev",
-      },
+      // headers: {
+      //   Host: "ticketing.dev",
+      // },
+      headers: req.headers,
     }
   );
 
