884f3d7cdb69845ea7261a0f77eea9b9897d84d2
fujinugraha16 <fujinugraha16@gmail.com>
2021-08-22 22:23:58 +0700

    create show route

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -5,6 +5,7 @@ import cookieSession from "cookie-session";
 
 // routers
 import { createTicketRouter } from "./routes/new";
+import { showTicketRouter } from "./routes/show";
 
 // middlewares and errors
 import {
@@ -26,6 +27,7 @@ app.use(
 app.use(currentUser);
 
 app.use(createTicketRouter);
+app.use(showTicketRouter);
 
 app.all("*", async () => {
   throw new NotFoundError();
