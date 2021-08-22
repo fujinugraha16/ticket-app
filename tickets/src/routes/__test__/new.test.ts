dd4e0b1958f67b11351fb495965b3b5cb119dbf6
fujinugraha16 <fujinugraha16@gmail.com>
2021-08-22 22:10:26 +0700

    creation via route handler

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -1,6 +1,7 @@
 import request from "supertest";
 import { app } from "../../app";
 import { signin } from "../../helpers/auth-test";
+import { Ticket } from "../../models/ticket";
 
 it("has a route handler listening to /api/tickets for post request", async () => {
   const response = await request(app).post("/api/tickets").send({});
@@ -60,12 +61,23 @@ it("returns an error if an invalid price is provided", async () => {
 });
 
 it("creates a ticket with valid input", async () => {
-  // add in a check to make sure a ticket was saved
+  let tickets = await Ticket.find({});
+  expect(tickets.length).toEqual(0);
+
+  const title = "asdfad";
+  const price = 20;
+
   await request(app)
     .post("/api/tickets")
+    .set("Cookie", signin())
     .send({
-      title: "asdfas",
-      price: 20,
+      title,
+      price,
     })
     .expect(201);
+
+  tickets = await Ticket.find({});
+  expect(tickets.length).toEqual(1);
+  expect(tickets[0].title).toEqual(title);
+  expect(tickets[0].price).toEqual(price);
 });
--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -2,6 +2,9 @@ import express, { Request, Response } from "express";
 import { body } from "express-validator";
 import { requireAuth, validateRequest } from "@fujingrtickets/common";
 
+// models
+import { Ticket } from "../models/ticket";
+
 const router = express.Router();
 
 router.post(
@@ -14,8 +17,17 @@ router.post(
       .withMessage("Price must be greater than 0"),
   ],
   validateRequest,
-  (req: Request, res: Response) => {
-    res.sendStatus(200);
+  async (req: Request, res: Response) => {
+    const { title, price } = req.body;
+
+    const ticket = Ticket.build({
+      title,
+      price,
+      userId: req.currentUser!.id,
+    });
+    await ticket.save();
+
+    res.status(201).send(ticket);
   }
 );
 
