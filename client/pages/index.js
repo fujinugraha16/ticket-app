2d186478edfdded2ae1edf578226bd711ef527f4
fujinugraha16 <fujinugraha16@gmail.com>
2021-08-22 13:37:22 +0700

    integrate with service in other namespace and specifying the host

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,10 +1,26 @@
-const LandingPage = () => {
+import axios from "axios";
+
+const LandingPage = ({ currentUser }) => {
+  console.log(currentUser);
+
   return <h1>Landing Page</h1>;
 };
 
-export const getStaticProps = async () => {
+export const getServerSideProps = async () => {
+  const { data } = await axios.get(
+    // http://SERVICE_NAME.NAMESPACE.svc.cluster.local/
+    "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser",
+    {
+      headers: {
+        Host: "ticketing.dev",
+      },
+    }
+  );
+
   return {
-    props: {},
+    props: {
+      ...data,
+    },
   };
 };
 
