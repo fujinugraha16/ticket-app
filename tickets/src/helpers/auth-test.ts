164f44a2c0e57390272827cdfcad70cc7538c507
fujinugraha16 <fujinugraha16@gmail.com>
2021-08-22 21:35:06 +0700

    building a session

--- tickets/src/helpers/auth-test.ts
+++ tickets/src/helpers/auth-test.ts
@@ -1,12 +1,26 @@
-import request from "supertest";
-import { app } from "../app";
+import jwt from "jsonwebtoken";
 
-export const signin = async () => {
+export const signin = () => {
+  // Fake Cookie for test
   // Build a JWT payload. {id, email}
   // eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxMjI1YzZjYzAyMGZiMDAyZTVmNzU3NiIsImVtYWlsIjoidGVzdDJAdGVzdC5jb20iLCJpYXQiOjE2Mjk2NDE4MzZ9.mPdFWLNNtFodUQYesfUtOD1z7Txt9ZBfHynl9kcm2cU
+  const payload = {
+    id: "6434234adfa32",
+    email: "test@test.com",
+  };
+
   // Create the JWT!
+  const token = jwt.sign(payload, process.env.JWT_KEY!);
+
   // Build session object. {jwt: MY_JWT}
+  const session = { jwt: token };
+
   // Turn that session into JSON
+  const sessionJSON = JSON.stringify(session);
+
   // Take JSON and encoded it as base64
+  const base64 = Buffer.from(sessionJSON).toString("base64");
+
   // return a string thats the cookie with the encoded data
+  return [`express:sess=${base64}`];
 };
--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -1,5 +1,6 @@
 import request from "supertest";
 import { app } from "../../app";
+import { signin } from "../../helpers/auth-test";
 
 it("has a route handler listening to /api/tickets for post request", async () => {
   const response = await request(app).post("/api/tickets").send({});
@@ -12,7 +13,10 @@ it("can only be accessed if the user is signed in", async () => {
 });
 
 it("retures a status other than 401 if the user is signed in", async () => {
-  const response = await request(app).post("/api/tickets").send({});
+  const response = await request(app)
+    .post("/api/tickets")
+    .set("Cookie", signin())
+    .send({});
 
   expect(response.status).not.toEqual(401);
 });
