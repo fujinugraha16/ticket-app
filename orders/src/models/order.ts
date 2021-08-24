5f308cd1ae6e68414b20d8bb50bc0ad165174b5a
fujinugraha16 <fujinugraha16@gmail.com>
2021-08-24 21:53:54 +0700

    creating an order status enum

--- orders/src/models/order.ts
+++ orders/src/models/order.ts
@@ -1,15 +1,16 @@
 import mongoose from "mongoose";
+import { OrderStatus } from "@fujingrtickets/common";
 
 interface OrderAttrs {
   userId: string;
-  status: string;
+  status: OrderStatus;
   expiresAt: Date;
   ticket: TicketDoc;
 }
 
 interface OrderDoc extends mongoose.Document {
   userId: string;
-  status: string;
+  status: OrderStatus;
   expiresAt: Date;
   ticket: TicketDoc;
 }
@@ -27,6 +28,7 @@ const orderSchema = new mongoose.Schema(
     status: {
       type: String,
       required: true,
+      enum: Object.values(OrderStatus),
     },
     expiresAt: {
       type: mongoose.Schema.Types.Date,
