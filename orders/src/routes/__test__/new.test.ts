6cbf809cc087d78a66dd4494bd865ea64c93100d
fujinugraha16 <fujinugraha16@gmail.com>
2021-08-25 09:43:47 +0700

    testing the success case

--- orders/src/routes/__test__/new.test.ts
+++ orders/src/routes/__test__/new.test.ts
@@ -41,4 +41,16 @@ it("returns an error if the ticket already reserved", async () => {
     .expect(400);
 });
 
-it("reserve a ticket", async () => {});
+it("reserve a ticket", async () => {
+  const ticket = Ticket.build({
+    title: "concert",
+    price: 20,
+  });
+  await ticket.save();
+
+  await request(app)
+    .post("/api/orders")
+    .set("Cookie", signin())
+    .send({ ticketId: ticket.id })
+    .expect(201);
+});
