8c089bf21d86c0a5d239bf34540504ee0b21c2ed
fujinugraha16 <fujinugraha16@gmail.com>
2021-08-26 20:55:01 +0700

    emitting the order cancelled event

--- orders/src/events/listener/expiration-complete-listener.ts
+++ orders/src/events/listener/expiration-complete-listener.ts
@@ -10,12 +10,15 @@ import { queueGroupName } from "./queue-group-name";
 // models
 import { Order } from "../../models/order";
 
+// events
+import { OrderCancelledPublisher } from "../publisher/order-cancelled-publisher";
+
 export class ExpirationCompleteListener extends Listener<ExpirationCompleteEvent> {
   readonly subject = Subjects.ExpirationComplete;
   queueGroupName = queueGroupName;
 
   async onMessage(data: ExpirationCompleteEvent["data"], msg: Message) {
-    const order = await Order.findById(data.orderId);
+    const order = await Order.findById(data.orderId).pupulate("ticket");
 
     if (!order) {
       throw new Error("Order not found");
@@ -23,7 +26,18 @@ export class ExpirationCompleteListener extends Listener<ExpirationCompleteEvent
 
     order.set({
       status: OrderStatus.Cancelled,
-      ticket: null,
     });
+    await order.save();
+
+    // publish
+    await new OrderCancelledPublisher(this.client).publish({
+      id: order.id,
+      ticket: {
+        id: order.ticket.id,
+      },
+      version: order.version,
+    });
+
+    msg.ack();
   }
 }
