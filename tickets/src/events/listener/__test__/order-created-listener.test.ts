29e1c418835d2e9ee356c1626f11ce39ce2f6c10
fujinugraha16 <fujinugraha16@gmail.com>
2021-08-26 16:25:16 +0700

    order cancelled listener

--- tickets/src/events/listener/__test__/order-created-listener.test.ts
+++ tickets/src/events/listener/__test__/order-created-listener.test.ts
@@ -49,7 +49,7 @@ it("sets the userId of the ticket", async () => {
 });
 
 it("acks the message", async () => {
-  const { listener, ticket, data, msg } = await setup();
+  const { listener, data, msg } = await setup();
 
   await listener.onMessage(data, msg);
 
