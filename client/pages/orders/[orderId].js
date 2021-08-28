179decaa5cbbeb2c76f9e3c02fe8e76de35d7e66
fujinugraha16 <fujinugraha16@gmail.com>
2021-08-28 11:14:41 +0700

    the expiration timer

--- client/pages/orders/[orderId].js
+++ client/pages/orders/[orderId].js
@@ -1,5 +1,36 @@
-const OrderShow = () => {
-  return <div>OrderShow</div>;
+import { useEffect, useState } from "react";
+import buildClient from "../../api/build-client";
+
+const OrderShow = ({ order }) => {
+  const [timeLeft, setTimeLeft] = useState("");
+
+  useEffect(() => {
+    const findTimeLeft = () => {
+      const msLeft = new Date(order.expiresAt) - new Date();
+      setTimeLeft(Math.round(msLeft / 1000));
+    };
+
+    findTimeLeft();
+    const timerId = setInterval(findTimeLeft, 1000);
+
+    return () => {
+      clearInterval(timerId);
+    };
+  }, [order]);
+
+  return <div>Time left to pay: {timeLeft} seconds</div>;
+};
+
+export const getServerSideProps = async (context) => {
+  const { orderId } = context.query;
+  const client = buildClient(context);
+  const { data } = await client.get(`/api/orders/${orderId}`);
+
+  return {
+    props: {
+      order: data,
+    },
+  };
 };
 
 export default OrderShow;
